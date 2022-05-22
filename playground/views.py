81e6f8befb734cc637f64a8ccd11153ae3a7908e
saadamir-1 <samir.bscs18seecs@seecs.edu.pk>
2022-05-23 04:49:13 +0500

    cloud sync

--- playground/views.py
+++ playground/views.py
@@ -21,6 +21,19 @@ from playground.comparison import comparisons
 # action ( views is alse called action in some frameworks)
 class Arguments:
     def __init__(self):
+        self.data_path = '/mnt/efs/fs1/proj/storefront/landsat_files/'
+        self.rasterized_shapefiles_path = '/mnt/efs/fs1/proj/storefront/shape_files_org/' 
+        self.model_topology = 'ENC_4_DEC_4' 
+        self.bands = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] 
+        self.classes = ['Non-Forest', 'Forest'] 
+        self.model_path = '/mnt/efs/fs1/test_data/model_99.pt' 
+        self.dest = '/mnt/efs/fs1/proj/storefront/playground/inference_results/'
+        self.bs = 4 
+        self.cuda = 0
+        self.device = 0
+
+class Arguments2:
+    def __init__(self):
         self.data_path = '/mnt/efs/fs1/proj/storefront/'
         self.rasterized_shapefiles_path = '/mnt/efs/fs1/proj/storefront/shapefiles/' 
         self.model_topology = 'ENC_4_DEC_4' 
@@ -41,7 +54,7 @@ class Arguments_m2m:
 def run_project(request, startYear, endYear, region):
 
     args = Arguments()
-    args_downloading = Arguments_m2m() 
+    # args_downloading = Arguments_m2m() 
 
     if startYear == endYear:
         year_our = [startYear]
@@ -51,38 +64,7 @@ def run_project(request, startYear, endYear, region):
     print("\n**************************************\n")
     print("-----------> Activity Log <-----------")
     print("\n**************************************\n")
-    print("\n==> Downloading ShapeFiles")
-
-    shapeFileDownload(73.1003, 34.2549, 73.3859, 34.2504, 73.4024, 34.0640, 73.0618, 34.0709, region)
-
-    print("\n==> Downloading Complete")
-    print("\n==> Creating bounding box")
-
-    bbox1 = findBBox(73.1003, 34.2549, 73.3859, 34.2504, 73.4024, 34.0640, 73.0618, 34.0709)
-
-    print("\n==> Bounding box created")
-    print("\n==> Searching for Landsat8 Images")
-
-    image_names = searchImage(bbox1, year_our)
-
-    print("\n==> Search completed for Landsat8 Images\n")
-    count = 0
-    print(image_names) 
-    for x in image_names:
-       print("==> Editing scenes file for" + x +"\n")
-       editTxt(x)
-       run_m2m(args_downloading)
-       if count == 0:
-           stack(x, startYear, region)
-           count+=count
-       elif count == 1:
-           stack(x, endYear, region)
-    
-    print("\n==> Converting Shapefile")
-    shpToRaster(startYear, region)
-
-    #Inference
-    print("\n==> RUNNING INFERENCE")
+    print("\n==> STARTING INFERENCE")
 
     region_our = [region]
     inference_btt_2020.run_inference(args, year_our, region_our)
@@ -91,7 +73,7 @@ def run_project(request, startYear, endYear, region):
     if startYear != endYear:
         print("\n==> RUNNING COMPARISON")
 
-        comparison(startYear, endYear, region)
+        comparisons(startYear, endYear, region)
         imagePath = '/mnt/efs/fs1/proj/storefront/playground/inference_results/' + startYear + '_' + endYear+'_' + region + '_comparison.png'
         try:
             with open(imagePath, "rb") as f:
@@ -125,7 +107,7 @@ def run_project(request, startYear, endYear, region):
 
 def run_project_map(request, startYear, endYear, region, a1, a2, b1, b2, c1, c2, d1, d2):
 
-    args = Arguments()
+    args = Arguments2()
     args_downloading = Arguments_m2m() 
 
     if startYear == endYear:
